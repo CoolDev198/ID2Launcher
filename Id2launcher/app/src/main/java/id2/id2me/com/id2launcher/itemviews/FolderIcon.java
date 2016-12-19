ec3b4fc7d48556d60f6f5b7f8710cc08288a50bd
Sunita <sunitasahu@crazyinnotech.com>
2016-12-19 20:52:08 +0530

    Folder Icon
    hardcode number removed and drawable list of app in folder

--- Id2launcher/app/src/main/java/id2/id2me/com/id2launcher/itemviews/FolderIcon.java
+++ Id2launcher/app/src/main/java/id2/id2me/com/id2launcher/itemviews/FolderIcon.java
@@ -6,7 +6,11 @@ import android.graphics.Color;
 import android.graphics.PorterDuff;
 import android.graphics.drawable.Drawable;
 import android.support.v4.content.ContextCompat;
+import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 import id2.id2me.com.id2launcher.R;
 
@@ -15,6 +19,7 @@ import id2.id2me.com.id2launcher.R;
  */
 
 public class FolderIcon extends LinearLayout {
+    private static final int NUM_ITEMS_IN_PREVIEW = 3;
     private static final float PERSPECTIVE_SCALE_FACTOR = 1f;
     private static final float PERSPECTIVE_SHIFT_FACTOR = 1f;
     private float mMaxPerspectiveShift;
@@ -26,6 +31,7 @@ public class FolderIcon extends LinearLayout {
     private Drawable drawable;
     private int mPreviewOffsetX;
     private int mPreviewOffsetY;
+    //private Folder mFolder;
 
     private PreviewItemDrawingParams mParams = new PreviewItemDrawingParams(0, 0, 0, 0);
 
@@ -36,20 +42,29 @@ public class FolderIcon extends LinearLayout {
     @Override
     protected void dispatchDraw(Canvas canvas) {
         super.dispatchDraw(canvas);
+        //ArrayList<View> items = mFolder.getItemsInReadingOrder(false);
+        ArrayList<View> items = new ArrayList<>();  // get app list in Folder
 
-        Drawable d = ContextCompat.getDrawable(getContext(), R.mipmap.ic_launcher);
+        Drawable d;
+        TextView v;
+        int counter = 0;
+        v = (TextView) items.get(0);
+        d = v.getCompoundDrawables()[1];
         computePreviewDrawingParams(d);
 
         float transX = 0;
         float transY = 0;
 
-        for(int i = 0 ; i < 3 ; i++) {
+        for(int i = 0 ; i < NUM_ITEMS_IN_PREVIEW ; i++) {
             transX = mBaselineIconSize * i;
-            for(int j = 0 ; j < 3 ; j++) {
+            for(int j = 0 ; j < NUM_ITEMS_IN_PREVIEW ; j++) {
                 transY = mBaselineIconSize * j;
+                v = (TextView) items.get(counter);
+                d = v.getCompoundDrawables()[1];
                 mParams = computePreviewItemDrawingParams(transX, transY ,mParams);
                 mParams.drawable = d;
                 drawPreviewItem(canvas, mParams);
+                counter++;
             }
         }
     }
@@ -102,11 +117,6 @@ public class FolderIcon extends LinearLayout {
 
         float r = 0.75f;
         float scale = (1 - PERSPECTIVE_SCALE_FACTOR * (1 - r));
-
-        // We want to imagine our coordinates from the bottom left, growing up and to the
-        // right. This is natural for the x-axis, but for the y-axis, we have to invert things.
-        /*float transY = mAvailableSpaceInPreview - (offset + scaledSize + scaleOffsetCorrection);
-        float transX = offset + scaleOffsetCorrection;*/
 
         float totalScale = mBaselineIconScale * scale;
         final int overlayAlpha = (int) (80 * (1 - r));
