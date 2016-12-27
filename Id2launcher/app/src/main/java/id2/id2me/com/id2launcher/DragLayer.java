9541b0175bcdfc8240c0589bb34bc4779dd20ba1
Deepak <deepaksakhare@crazyinnotech.com>
2016-12-27 18:56:48 +0530

    Layoutparams added for resize of widget

--- Id2launcher/app/src/main/java/id2/id2me/com/id2launcher/DragLayer.java
+++ Id2launcher/app/src/main/java/id2/id2me/com/id2launcher/DragLayer.java
@@ -389,5 +389,49 @@ public class DragLayer extends FrameLayout implements ViewGroup.OnHierarchyChang
     public View getAnimatedView() {
         return mDropView;
     }
+
+    public static class LayoutParams extends FrameLayout.LayoutParams {
+        public int x, y;
+        public boolean customPosition = false;
+
+        /**
+         * {@inheritDoc}
+         */
+        public LayoutParams(int width, int height) {
+            super(width, height);
+        }
+
+        public void setWidth(int width) {
+            this.width = width;
+        }
+
+        public int getWidth() {
+            return width;
+        }
+
+        public void setHeight(int height) {
+            this.height = height;
+        }
+
+        public int getHeight() {
+            return height;
+        }
+
+        public void setX(int x) {
+            this.x = x;
+        }
+
+        public int getX() {
+            return x;
+        }
+
+        public void setY(int y) {
+            this.y = y;
+        }
+
+        public int getY() {
+            return y;
+        }
+    }
 }
 
