bc6d51a36bc579eea59306ea01adeebfb8e71639
Deepak <deepaksakhare@crazyinnotech.com>
2017-01-05 14:08:38 +0530

    DeleteDropTarget added

---
+++ Id2launcher/app/src/main/java/id2/id2me/com/id2launcher/DeleteDropTarget.java
@@ -0,0 +1,19 @@
+package id2.id2me.com.id2launcher;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+/**
+ * Created by CrazyInnoTech on 05-01-2017.
+ */
+
+public class DeleteDropTarget extends ButtonDropTarget {
+
+    public DeleteDropTarget(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public DeleteDropTarget(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
