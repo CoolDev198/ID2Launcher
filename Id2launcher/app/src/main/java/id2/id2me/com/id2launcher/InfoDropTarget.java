27bf072b5284f2be6f40605a63e719a00666aef5
Deepak <deepaksakhare@crazyinnotech.com>
2017-01-05 14:08:51 +0530

    InfoDropTarget added

---
+++ Id2launcher/app/src/main/java/id2/id2me/com/id2launcher/InfoDropTarget.java
@@ -0,0 +1,19 @@
+package id2.id2me.com.id2launcher;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+/**
+ * Created by CrazyInnoTech on 05-01-2017.
+ */
+
+public class InfoDropTarget extends ButtonDropTarget {
+
+    public InfoDropTarget(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public InfoDropTarget(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
