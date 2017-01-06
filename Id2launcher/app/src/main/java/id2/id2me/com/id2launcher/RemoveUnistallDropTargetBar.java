1b983402c6a1ebc3d15f1c508b4c73daea20f0b5
Deepak <deepaksakhare@crazyinnotech.com>
2017-01-06 12:13:08 +0530

    info and remove/uninstall inflated in droptarget

--- Id2launcher/app/src/main/java/id2/id2me/com/id2launcher/RemoveUnistallDropTargetBar.java
+++ Id2launcher/app/src/main/java/id2/id2me/com/id2launcher/RemoveUnistallDropTargetBar.java
@@ -2,6 +2,7 @@ package id2.id2me.com.id2launcher;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.View;
 import android.widget.FrameLayout;
 
 /**
@@ -10,12 +11,38 @@ import android.widget.FrameLayout;
 
 public class RemoveUnistallDropTargetBar extends FrameLayout implements DragController.DragListener {
 
+    private ButtonDropTarget mInfoDropTarget;
+    private ButtonDropTarget mDeleteDropTarget;
+
+
     public RemoveUnistallDropTargetBar(Context context, AttributeSet attrs) {
         this(context, attrs, 0);
     }
 
     public RemoveUnistallDropTargetBar(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+    }
+
+    public void setup(Launcher launcher, DragController dragController) {
+        //dragController.addDragListener(this);
+        /*dragController.addDragListener(mInfoDropTarget);
+        dragController.addDragListener(mDeleteDropTarget);*/
+        dragController.addDropTarget(mInfoDropTarget);
+        dragController.addDropTarget(mDeleteDropTarget);
+        //dragController.setFlingToDeleteDropTarget(mDeleteDropTarget);
+        /*mInfoDropTarget.setLauncher(launcher);
+        mDeleteDropTarget.setLauncher(launcher);*/
+    }
+
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+
+        // Get the individual components
+
+        mInfoDropTarget = (ButtonDropTarget) findViewById(R.id.info_target_text);
+        mDeleteDropTarget = (ButtonDropTarget) findViewById(R.id.remove_target_text);
+
     }
 
     @Override
