15072f114dc40dcdc1d10f828db7c1e326f7cd76
sunita sahu <sunita@sunitas-MacBook-Pro.local>
2017-01-11 13:14:10 +0530

    folder fragment changed

--- Id2launcher/app/src/main/java/id2/id2me/com/id2launcher/fragments/FolderFragment.java
+++ Id2launcher/app/src/main/java/id2/id2me/com/id2launcher/fragments/FolderFragment.java
@@ -7,55 +7,36 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import java.util.ArrayList;
-
-import id2.id2me.com.id2launcher.DatabaseHandler;
 import id2.id2me.com.id2launcher.R;
-import id2.id2me.com.id2launcher.models.ItemInfo;
 
 /**
  * Created by bliss76 on 26/05/16.
  */
 public class FolderFragment extends Fragment {
-    static final String FolderId = "folderId";
-    final int NO_OF_APPS_IN_ROW = 3;
-    View fragmentView;
-    ArrayList<ItemInfo> itemInfoModels;
 
-    private DatabaseHandler db;
-    public long folderId;
     public static final FolderFragment newInstance(long folderId) {
         FolderFragment f = new FolderFragment();
         Bundle bundle = new Bundle();
-        bundle.putLong("folderId", folderId);
         f.setArguments(bundle);
         return f;
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        try {
-            fragmentView = inflater.inflate(R.layout.popup_view, container, false);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        View fragmentView = inflater.inflate(R.layout.user_folder, container, false);
+
         return fragmentView;
     }
 
     @Override
     public void onResume() {
         super.onResume();
-        //if (fragmentView != null) {
-         // updateView();
-      //  }
     }
+
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-        if (context != null)
-            db = DatabaseHandler.getInstance(context);
     }
 
     @Override
@@ -64,4 +45,4 @@ public class FolderFragment extends Fragment {
 
     }
 
-   }
+}
